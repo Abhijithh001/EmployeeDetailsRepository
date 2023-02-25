52a39fcc15f31bb6b0f51a4c68b676d98eaf380a
Abhijith <abhijithh91@gmail.com>
2023-02-25 12:19:55 +0530

    Hello

---
+++ MiniProject/Controllers/DemoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiniProject.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
